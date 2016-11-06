f32fff25ff8c420a387300531fd21629a3e89d2e
Fredrik Lindroth <fredrik.lindroth@egbs.se>
2016-11-06 23:33:07 +0100

    Added owner to stations and station listing.

--- src/main/java/se/uppsalamakerspace/iot/controller/admin/AdminStationController.java
+++ src/main/java/se/uppsalamakerspace/iot/controller/admin/AdminStationController.java
@@ -1,14 +1,19 @@
 package se.uppsalamakerspace.iot.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import se.uppsalamakerspace.iot.model.Station;
+import se.uppsalamakerspace.iot.model.User;
 import se.uppsalamakerspace.iot.repository.StationRepository;
+import se.uppsalamakerspace.iot.repository.UserRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -18,10 +23,12 @@ import java.util.UUID;
 public class AdminStationController {
 
     private final StationRepository stationRepo;
+    private final UserRepository userRepo;
 
     @Autowired
-    public AdminStationController(StationRepository stationRepo) {
+    public AdminStationController(StationRepository stationRepo, UserRepository userRepo) {
         this.stationRepo = stationRepo;
+        this.userRepo = userRepo;
     }
 
     @GetMapping("/admin/station")
@@ -36,7 +43,12 @@ public class AdminStationController {
     }
 
     @PostMapping("/admin/station")
-    public String addStation(@ModelAttribute Station station) {
+    public String addStation(Authentication auth, @ModelAttribute Station station) {
+        Optional<User> user = getUserByAuth(auth);
+        if(!user.isPresent()) {
+            throw new RuntimeException("You are logged in as an non-existent user. You cannot create a station.");
+        }
+        station.setOwner(user.get());
         station.setUuid(genUuid());
         stationRepo.save(station);
         return "redirect:/admin/station";
@@ -44,5 +56,16 @@ public class AdminStationController {
 
     private String genUuid() {
         return UUID.randomUUID().toString();
+    }
+
+    private Optional<User> getUserByAuth(Authentication authentication) {
+        if(authentication == null || !authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_USER"))) {
+            return Optional.empty();
+        }
+        User user = userRepo.findByUserName(authentication.getName());
+        if(user == null) {
+            return Optional.empty();
+        }
+        return Optional.of(user);
     }
 }
--- src/main/java/se/uppsalamakerspace/iot/model/Station.java
+++ src/main/java/se/uppsalamakerspace/iot/model/Station.java
@@ -1,9 +1,6 @@
 package se.uppsalamakerspace.iot.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created by fredl2 on 2016-11-01.
@@ -19,6 +16,10 @@ public class Station {
     @Column(name = "station_name")
     private String name;
 
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinColumn(name = "station_owner", nullable = false)
+    private User owner;
+
     public String getUuid() {
         return uuid;
     }
@@ -33,5 +34,13 @@ public class Station {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
     }
 }
